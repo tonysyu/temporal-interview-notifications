35ca37f620a2b84e32ceb5d8cd1bd8e10d4944c3
Tony S Yu <tsyu80@gmail.com>
2022-12-13 21:03:17 -0600

    Reformat source to use 4-space indentation

--- src/workflows.ts
+++ src/workflows.ts
@@ -3,10 +3,10 @@ import { proxyActivities } from '@temporalio/workflow';
 import type * as activities from './activities';
 
 const { greet } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '1 minute',
+    startToCloseTimeout: '1 minute',
 });
 
 /** A workflow that simply calls an activity */
 export async function example(name: string): Promise<string> {
-  return await greet(name);
+    return await greet(name);
 }
